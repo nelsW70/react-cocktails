b87eb7859df24186f30aa6c2d7ab772a67d96875
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-11 10:19:02 -0400

    add: navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,13 @@ import Home from './pages/Home';
 import Error from './pages/Error';
 import About from './pages/About';
 import SingleCocktail from './pages/SingleCocktail';
+//navbar
+import Navbar from './components/Navbar';
 
 export default function App() {
   return (
     <Router>
+      <Navbar />
       <Switch>
         <Route exact path="/">
           <Home />
