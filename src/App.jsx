ce49e09646ea7afd459be9861e71f79ebaf8fe54
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-11 07:56:15 -0400

    add: import pages in app.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React from 'react';
+
+// import pages
+import Home from './pages/Home';
+import Error from './pages/Error';
+import About from './pages/About';
+import SingleCocktail from './pages/SingleCocktail';
 
 export default function App() {
   return <h1>app component</h1>;
