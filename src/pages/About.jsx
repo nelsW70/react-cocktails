ae39b9186d03c49be7155bf50bd521055bb8715b
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-11 10:21:29 -0400

    add: about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,21 @@
-import React from "react";
+import React from 'react';
 
 export default function About() {
-  return <h1>about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        I'm baby raw denim church-key keytar, tacos typewriter butcher taxidermy
+        distillery banh mi chambray. Tofu leggings readymade taxidermy coloring
+        book street art tbh food truck seitan pour-over succulents. Art party
+        ethical food truck everyday carry cornhole biodiesel chicharrones synth
+        cliche vexillologist thundercats af taiyaki. Tofu marfa pabst tote bag
+        cred flexitarian aesthetic chambray lomo hot chicken. Kombucha aesthetic
+        venmo leggings poke yuccie ugh fam lumbersexual blue bottle forage.
+        Paleo cardigan four loko brunch. Affogato shabby chic air plant
+        helvetica austin iPhone beard hashtag disrupt cronut craft beer heirloom
+        skateboard.
+      </p>
+    </section>
+  );
 }
